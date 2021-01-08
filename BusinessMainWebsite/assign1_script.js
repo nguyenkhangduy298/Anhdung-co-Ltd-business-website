677e6b2fc3238a840f6e3db774807f93fa32d5a0
nguyenkhangduy <nguyenkhangduy298@gmail.com>
2021-01-08 18:05:48 +0700

    Complete Project Commit

---
+++ BusinessMainWebsite/assign1_script.js
@@ -0,0 +1,46 @@
+// Information here is collected from https://fact-depot.com/dung-cu-hut-mau-1-kenh-pipet-lite-xls-dau-tiep-lts-mettler-toledo-l-100xls-p33401.html, https://nguyenanhvn.com/be-on-nhiet-memmert-wnb22.html, https://www.testoshop.vn/may-do-nhiet-do-testo-735-2.html source and used for educational purposes only
+var products = [
+    { id: "pro1",
+    name: "Thiết bị đo nhiệt độ 3 kênh Testo 735-2",
+    code: "0563 7352",
+    image: "product.png",
+    descrip: "Nhà sản xuất: Testo- Đức<br>Xuất xứ: Đức<br><br>Máy đo nhiệt độ testo 735-2 là thiết bị đo nhiệt đa kênh số được sử dụng rộng rãi trong nhiều lĩnh vực khác nhau. Nó có thể đo nhiệt độ bề mặt, nhiệt độ không khí, nhiệt độ lõi trong nhiều môi trường với độ chính xác lên đến 0.05°C sử dụng cảm biến dạng nhúng/đâm xuyên loại Pt độ chính xác cao. Thiết bị có thể kết nối không dây với 3 cảm biến nhiệt độ bên ngoài và giá trị đo của chúng được truyền tới màn hình do đó thiết bị có tới 6 kênh đo. <br> 1) Đo nhiệt độ kiểu PT100<br>Dải đo:   -200 ~ +800 °C<br>Độ chính xác:  ±0.2 °C (-100 ~ +199.9 °C)<br>2) Đo nhiệt độ kiểu K TC<br>Dải đo: -200 ~ +1370 °C<br>Độ chính xác: ±0.3 °C (-60 ~ +60 °C)<br>3) Đo nhiệt độ TC kiểu T (Cu-CuNi)<br>Dải đo: -200 ~ +400 °C<br>Độ chính xác: ±0.3 °C (-60 ~ +60 °C)<br>4) Đo nhiệt độ kiểu J TC<br>Dải đo: -200 ~ +1000 °C<br>Độ chính xác: ±0.3 °C (-60 ~ +60 °C)<br>5) Đo nhiệt độ TC kiểu S (Pt10Rh-Pt)<br>Dải đo: 0 ~ +1760 °C<br>Độ chính xác: ±1 °C (0 ~ +1760 °C)<br>6) Thông số chung<br>Vỏ khung: ABS / TPE / Metal<br>Cấp bảo vệ: IP65<br>Tiêu chuẩn: EN 13485<br>Kiểu pin: Alkaline, kiểu AA<br>Màn hình: LCD<br>Kích thước: 220 x 74 x 46 mm<br>Trọng lượng: 428 g<br>Phụ kiện: Máy chính Testo 735-2, phần mềm PC, dây USB, pin"},
+    
+    { id: "pro2" ,
+     name: "Bể Cách Thủy",
+     code: "WNB22",
+     image: "product2.png",
+     descrip: "Hãng sản xuất: Memmert – Đức<br>Xuất xứ: Đức<br><br>Sử dụng trong phòng thí nghiệm công nghệ thực phẩm và mỹ phẩm, các phòng thí nghiệm nghiên cứu như: hóa học, sinh học, vi sinh, hóa sinh và các phòng thí nghiệm lâm sàng<br>-     Thể tích: khoảng 22 lít<br>-     Kích thước trong: dài 350 x rộng 290 x cao 220 mm<br>-     Kích thước ngoài: dài 578 x rộng 436 x cao 296 mm<br>-     Mức chất lỏng tối thiểu/tối đa: 177/200 mm<br>-     Khoảng nhiệt độ hoạt động: tối thiểu 5 0 C trên nhiệt độ môi trường đến 95 0 C với thêm chế độ điểm sôi(+100 0 C)<br>-     Độ biến thiên: ± 0.1 0 C<br>-     Độ phân bố: ± 0.25 0 C<br>-     Bộ điều khiển: hiển thị số (LED) cho nhiệt độ cài đặt và nhiệt độ thực (độ phân giải 0.1 0 C) và thời gian còn lại<br>-     Nhiệt độ: cảm biến Pt100 class A<br>-     Cài đặt thời gian: bộ cài đặt bằng kỹ thuật số từ 1 phút đến 99 giờ 59 phút cho: ON (hoạt động liên tục), WAIT ((chức năng chờ cho hoạt động liên tục và hoạt động theo thời gian), HOLD (chức năng giữ nhiệt)<br>-     Kiểm soát nhiệt: bộ giới hạn nhiệt độ bằng cơ TB cấp bảo vệ class 1 ngắt chế độ gia nhiệt khi vượt quá khoảng 30 0 C trên nhiệt độ tối đa của bể<br>-     Kiểm soát nhiệt: trong trường hợp quá nhiệt do sự cố, chế độ gia nhiệt được ngắt ở khoảng 10 0 C trên nhiệt độ cài đặt (giá trị cố định)<br>-     Hệ thống tự chẩn đoán: bộ điều khiển nhiệt độ bằng vi xử lý PID có tích hợp hệ thống tự động chẩn đoán chỉ thị báo lỗi<br>-     Bể gia nhiệt: gia nhiệt trên bề mặt diện tích lớn ba mặt<br>-     Bên trong bằng thép không gỉ làm bằng vật liệu 1.4301 (ASTM 304) được hàn bằng công nghệ laser, dễ dàng vệ sinh<br>-     Bên ngoài bằng thép không gỉ<br>-     Nguồn điện: 230 V, 50/60 Hz<br>-     Công suất: khoảng 2000 W (trong quá trình gia nhiệt)<br>-     Nhiệt độ môi trường: +5 0 C đến 40 0 C<br>-     Độ ẩm: tối đa 80% rh, không ngưng tụ"},
+
+    { id: "pro3", 
+     image: "product3.png",
+     name: "Micropipette – dụng cụ hút mẫu định lượng L-1000XLS+-", 
+     code: "17014382",
+     descrip: "Hãng sản xuất: Mettler Toledo – Thụy Sỹ<br><br>Xuất xứ: USA<br>- Thang thể tích: 100-1000μL<br>- Loại: 1 kênh - Có khóa điều chỉnh thể tích <br>- Vạch chia: 2μL<br>- Độ chính xác:<br>+ Tại 100μL: 3.0μL<br>+ Tại 500μL: 4.0μL<br>+ Tại 1000μL: 8.0μL<br>- Loại đầu tip sử dụng: LTS<br>- Bảo hành: 12 tháng<br>- Cung cấp bao gồm: Máy chính, Hướng dẫn sử dụng" 
+     }
+];
+
+function loadInfo(item) {
+    document.getElementById("product-info").hidden = false;
+	switch(item.id) {
+	case products[0].id:
+        document.getElementById("product-name").innerHTML = products[0].name;
+        document.getElementById("product-code").innerHTML = products[0].code;
+        document.getElementById("product-descrip").innerHTML = products[0].descrip;
+        document.getElementById("product-pic").src = products[0].image;
+		break;
+    
+	case products[1].id:
+        document.getElementById("product-name").innerHTML = products[1].name;
+        document.getElementById("product-code").innerHTML = products[1].code;
+        document.getElementById("product-descrip").innerHTML = products[1].descrip;
+        document.getElementById("product-pic").src = products[1].image;
+		break;
+	case products[2].id:
+		document.getElementById("product-name").innerHTML = products[2].name;
+        document.getElementById("product-code").innerHTML = products[2].code;
+        document.getElementById("product-descrip").innerHTML = products[2].descrip;
+        document.getElementById("product-pic").src = products[2].image;
+		break;
+	}
+}
